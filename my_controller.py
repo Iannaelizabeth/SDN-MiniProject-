91c2f71e35c832fee9b42e7ebfa2dde9349a9017
Iannaelizabeth <ianna.elizabeth.reni2gmail.com>
2026-04-09 22:42:57 +0000

    Initial commit

---
+++ my_controller.py
@@ -0,0 +1,39 @@
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+
+log = core.getLogger()
+
+mac_to_port = {}
+
+def _handle_PacketIn(event):
+    packet = event.parsed
+    if not packet.parsed:
+        return
+
+    dpid = event.connection.dpid
+
+    if dpid not in mac_to_port:
+        mac_to_port[dpid] = {}
+
+    mac_to_port[dpid][packet.src] = event.port
+
+    if packet.dst in mac_to_port[dpid]:
+        out_port = mac_to_port[dpid][packet.dst]
+
+        msg = of.ofp_flow_mod()
+        msg.match = of.ofp_match.from_packet(packet)
+        msg.actions.append(of.ofp_action_output(port=out_port))
+
+        event.connection.send(msg)
+
+    else:
+        msg = of.ofp_packet_out()
+        msg.data = event.ofp
+        msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+
+        event.connection.send(msg)
+
+
+def launch():
+    log.info("Custom Controller Running...")
+    core.openflow.addListenerByName("PacketIn", _handle_PacketIn)
